0fa93fa3606b94751153d8def538b22046489df0
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2016-11-17 01:54:45 -0600

    Implementar EstacionDAO

--- app/src/main/java/com/busme_conductor/models/DAO/EstacionDAO.java
+++ app/src/main/java/com/busme_conductor/models/DAO/EstacionDAO.java
@@ -4,15 +4,20 @@ import com.busme_conductor.interfaces.ConsultasBD;
 import com.busme_conductor.models.ConexionBD;
 import com.busme_conductor.models.DTO.Estacion;
 
+import org.postgis.PGgeometry;
+
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EstacionDAO implements ConsultasBD<Estacion> {
     private static final String SQL_INSERT = "INSERT INTO estaciones(id_ruta, geom, orden_estacion, es_terminal) VALUES (?, ?, ?, ?)";
     private static final String SQL_DELETE = "DELETE FROM estaciones WHERE id_estacion = ?";
     private static final String SQL_UPDATE = "UPDATE estaciones SET id_ruta = ?, geom = ?, orden_estacion = ?, es_terminal = ? WHERE id_estacion = ?";
-    private static final String SQL_LEERRUTA = "SELECT * FROM estaciones WHERE id_ruta = ?";
+    private static final String SQL_READ = "SELECT * FROM estaciones WHERE id_ruta = ?";
+    private static final String SQL_READALL = "SELECT * FROM estaciones";
     private static final ConexionBD conexion = ConexionBD.connect();
 
     @Override
@@ -75,11 +80,40 @@ public class EstacionDAO implements ConsultasBD<Estacion> {
 
     @Override
     public Estacion read(Object key) {
-        return null;
+        PreparedStatement ps;
+        ResultSet rs;
+        Estacion est = null;
+        try {
+            ps = conexion.getConnection().prepareStatement(SQL_READ);
+            ps.setString(1, key.toString());
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                est = new Estacion(rs.getInt(1), rs.getString(2), (PGgeometry) rs.getObject(3), rs.getInt(4), rs.getBoolean(5));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            conexion.closeConnection();
+        }
+        return est;
     }
 
     @Override
     public List<Estacion> readAll() {
-        return null;
+        PreparedStatement ps;
+        ResultSet rs;
+        ArrayList<Estacion> estaciones = new ArrayList();
+        try {
+            ps = conexion.getConnection().prepareStatement(SQL_READALL);
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                estaciones.add(new Estacion(rs.getInt(1), rs.getString(2), (PGgeometry) rs.getObject(3), rs.getInt(4), rs.getBoolean(5)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            conexion.closeConnection();
+        }
+        return estaciones;
     }
 }
