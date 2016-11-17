85865aa8a837ba8e38030b0705881c80a44108b2
Eduardo Rodriguez <eduardoalbertorg@gmail.com>
2016-11-17 01:02:59 -0600

    Implementar clases Estacion y Ruta

---
+++ app/src/main/java/com/busme_conductor/models/DTO/Estacion.java
@@ -0,0 +1,66 @@
+package com.busme_conductor.models.DTO;
+
+import org.postgis.PGgeometry;
+
+public class Estacion {
+    String idEstacion;
+    String idRuta;
+    PGgeometry geom;
+    int ordenEstacion;
+    boolean esTerminal;
+
+    public Estacion(String idRuta, PGgeometry geom, int ordenEstacion, boolean esTerminal) {
+        this.idRuta = idRuta;
+        this.geom = geom;
+        this.ordenEstacion = ordenEstacion;
+        this.esTerminal = esTerminal;
+    }
+
+    public Estacion(String idEstacion, String idRuta, PGgeometry geom, int ordenEstacion, boolean esTerminal) {
+        this.idEstacion = idEstacion;
+        this.idRuta = idRuta;
+        this.geom = geom;
+        this.ordenEstacion = ordenEstacion;
+        this.esTerminal = esTerminal;
+    }
+
+    public String getIdEstacion() {
+        return idEstacion;
+    }
+
+    public void setIdEstacion(String idEstacion) {
+        this.idEstacion = idEstacion;
+    }
+
+    public String getIdRuta() {
+        return idRuta;
+    }
+
+    public void setIdRuta(String idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public PGgeometry getGeom() {
+        return geom;
+    }
+
+    public void setGeom(PGgeometry geom) {
+        this.geom = geom;
+    }
+
+    public int getOrdenEstacion() {
+        return ordenEstacion;
+    }
+
+    public void setOrdenEstacion(int ordenEstacion) {
+        this.ordenEstacion = ordenEstacion;
+    }
+
+    public boolean isEsTerminal() {
+        return esTerminal;
+    }
+
+    public void setEsTerminal(boolean esTerminal) {
+        this.esTerminal = esTerminal;
+    }
+}
---
+++ app/src/main/java/com/busme_conductor/models/DTO/Ruta.java
@@ -0,0 +1,17 @@
+package com.busme_conductor.models.DTO;
+
+public class Ruta {
+    String idRuta;
+
+    public Ruta(String idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public String getIdRuta() {
+        return idRuta;
+    }
+
+    public void setIdRuta(String idRuta) {
+        this.idRuta = idRuta;
+    }
+}
